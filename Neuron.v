6afa8820f20468f9a7806e2f60ea2b001cf12423
Chester <chester.h4@gmail.com>
2021-11-23 01:36:27 -0800

    initial commit

---
+++ Neuron.v
@@ -0,0 +1,50 @@
+`timescale 1ns/100ps
+
+module Neuron(IN_PIXELS, IN_WEIGHTS, OUT, done, clk, rst);
+parameter NUM_INPUTS = 784;
+parameter PIXEL_WIDTH = 10; //10-0 (int-deci)
+parameter WEIGHT_WIDTH = 19; //1-18
+parameter OUTPUT_WIDTH = 26; //8-18
+input [NUM_INPUTS*PIXEL_WIDTH-1:0] IN_PIXELS;
+input [NUM_INPUTS*WEIGHT_WIDTH-1:0] IN_WEIGHTS;
+input clk;
+input rst;
+output [OUTPUT_WIDTH-1:0] OUT;
+output done;
+
+parameter NUM_OF_ACCUMULATORS = 16; //need 49x16 steps (was going to be 28x28 but 10 neurons is 280 multipliers, too many)
+parameter BATCH_SIZE = 49; //each accumulator handles 49 pixels
+//Now each Neuron will do 49 batches of 16 multipliers each.
+
+wire [OUTPUT_WIDTH*NUM_OF_ACCUMULATORS-1:0] temp_outputs;
+wire [NUM_OF_ACCUMULATORS-1:0] done_signals;
+reg [OUTPUT_WIDTH-1:0] out_buf; //total sum
+
+
+genvar i;
+generate
+    for (i = 0; i < NUM_OF_ACCUMULATORS; i=i+1) begin
+        PipelinedMultAccumulate mult_0(
+            .IN_PIXELS(IN_PIXELS[i*PIXEL_WIDTH*BATCH_SIZE +: PIXEL_WIDTH*BATCH_SIZE]),
+            .IN_WEIGHTS(IN_WEIGHTS[i*WEIGHT_WIDTH*BATCH_SIZE +: WEIGHT_WIDTH*BATCH_SIZE]),
+            .OUT(temp_outputs[i*OUTPUT_WIDTH +: OUTPUT_WIDTH]),
+            .clk(clk),
+            .rst(rst),
+            .done(done_signals[i])
+            );
+        defparam mult_0.NUM_INPUTS=BATCH_SIZE;
+    end
+endgenerate
+
+
+
+integer k;
+always @(temp_outputs) begin
+  out_buf = 0;
+  for (k=0; k<NUM_OF_ACCUMULATORS; k=k+1)
+    out_buf = out_buf + temp_outputs[k*OUTPUT_WIDTH +: OUTPUT_WIDTH];
+end
+assign OUT = out_buf;
+assign done = 0;//done_signals!=0;
+
+endmodule
---
+++ PipelinedMultAccumulate.v
@@ -0,0 +1,49 @@
+`timescale 1ns/100ps
+
+module PipelinedMultAccumulate(IN_PIXELS, IN_WEIGHTS, OUT, done, clk, rst);
+parameter NUM_INPUTS = 4;
+parameter PIXEL_WIDTH = 10; //10-0 (int-deci)
+parameter WEIGHT_WIDTH = 19; //1-18
+parameter OUTPUT_WIDTH = 26; //8-18
+input [NUM_INPUTS*PIXEL_WIDTH-1:0] IN_PIXELS;
+input [NUM_INPUTS*WEIGHT_WIDTH-1:0] IN_WEIGHTS;
+input clk;
+input rst;
+output [OUTPUT_WIDTH-1:0] OUT;
+output done;
+reg [15:0] step; //make sure this is wide enough to fit number of inputs
+
+reg [OUTPUT_WIDTH-1:0] out_buf;
+wire [OUTPUT_WIDTH-1:0] mult_output;
+wire [WEIGHT_WIDTH-1:0] current_weight;
+wire [PIXEL_WIDTH-1:0] current_pixel;
+
+assign done = step == NUM_INPUTS+1;
+assign OUT = out_buf;
+assign current_weight = (step < NUM_INPUTS+1) ? IN_WEIGHTS[(step-1)*WEIGHT_WIDTH +: WEIGHT_WIDTH] : 0;
+assign current_pixel = (step < NUM_INPUTS+1) ? IN_PIXELS[(step-1)*PIXEL_WIDTH +: PIXEL_WIDTH] : 0;
+
+
+FixedPointMultiplier mult_0(
+  .clk(clk),
+  .GlobalReset(rst),
+  .WeightPort(current_weight), // sfix19_En18
+  .PixelPort(current_pixel), // sfix10_En0
+  .Output_syn(mult_output) // sfix26_En18
+);
+
+always@(posedge clk) begin
+	if (rst) begin
+    step <= 0;
+    out_buf <= 0;
+  end else begin
+    out_buf <= mult_output + out_buf;
+    if (step < NUM_INPUTS+1) begin
+      step <= step + 1;
+    end else begin
+      step <= step;
+    end
+  end
+end
+
+endmodule
