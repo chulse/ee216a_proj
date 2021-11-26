2953b7a6cce78946920b1b2933a977e255f1154f
Michelle Tan <michellektan37@gmail.com>
2021-11-25 22:59:16 -0800

    signed comparison fix

--- Max_selector.v
+++ Max_selector.v
@@ -1,18 +1,29 @@
 module Max_selector(
-    input [25:0] image_number_0,
-    input [25:0] image_number_1,
-    input [25:0] image_number_2,
-    input [25:0] image_number_3,
-	input [25:0] image_number_4,
-	input [25:0] image_number_5,
-	input [25:0] image_number_6,
-	input [25:0] image_number_7,
-	input [25:0] image_number_8,
-	input [25:0] image_number_9,
+    input [25:0] img_number_0,
+    input [25:0] img_number_1,
+    input [25:0] img_number_2,
+    input [25:0] img_number_3,
+	input [25:0] img_number_4,
+	input [25:0] img_number_5,
+	input [25:0] img_number_6,
+	input [25:0] img_number_7,
+	input [25:0] img_number_8,
+	input [25:0] img_number_9,
 	input clk,
 	input rst,
 	output reg [3:0] max
     );
+
+assign image_number_0 = $signed(img_number_0);
+assign image_number_1 = $signed(img_number_1);
+assign image_number_2 = $signed(img_number_2);
+assign image_number_3 = $signed(img_number_3);
+assign image_number_4 = $signed(img_number_4);
+assign image_number_5 = $signed(img_number_5);
+assign image_number_6 = $signed(img_number_6);
+assign image_number_7 = $signed(img_number_7);
+assign image_number_8 = $signed(img_number_8);
+assign image_number_9 = $signed(img_number_9);
 
 always @(posedge clk) begin
 	if(rst)
